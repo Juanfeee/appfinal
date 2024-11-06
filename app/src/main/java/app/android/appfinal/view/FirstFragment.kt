7d599640df16b9a64827f6213f0a54628ef65579
Juanfeee <juanfelipebenavides71@gmail.com>
2024-11-05 22:56:35 -0500

    first commit

---
+++ app/src/main/java/app/android/appfinal/view/FirstFragment.kt
@@ -0,0 +1,36 @@
+package app.android.appfinal.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import app.android.appfinal.R
+import app.android.appfinal.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root=inflater.inflate(R.layout.fragment_first, container, false)
+
+        val btnGrabadora=root.findViewById<Button>(R.id.btnGrabadora)
+
+        btnGrabadora.setOnClickListener{
+            findNavController().navigate(R.id.action_firstFragment_to_grabadoraFragment)
+        }
+        return root
+    }
+
+
+}
---
+++ app/src/main/java/app/android/appfinal/view/GrabadoraFragment.kt
@@ -0,0 +1,27 @@
+package app.android.appfinal.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.android.appfinal.R
+
+
+class GrabadoraFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_grabadora, container, false)
+    }
+
+
+}
